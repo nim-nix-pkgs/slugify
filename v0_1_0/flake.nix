e0e2d15712d496f4b095361129279f4aec719e15
hugosenari <hugosenari@gmail.com>
2022-03-22 00:37:43 -0300

    chore: re-index ../nimpkgs/s/slugify

--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-slugify-v0_1_0.flake = false;
   inputs.src-slugify-v0_1_0.owner = "lenniezelk";
-  inputs.src-slugify-v0_1_0.ref   = "refs/tags/v0.1.0";
+  inputs.src-slugify-v0_1_0.ref   = "v0_1_0";
   inputs.src-slugify-v0_1_0.repo  = "slugify";
   inputs.src-slugify-v0_1_0.type  = "github";
   
